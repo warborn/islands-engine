82a8f44cd0e7998c0aacf1357a8585ee8c188da5
Ivan Munguia <ivanalejandro249@gmail.com>
2019-06-13 18:34:30 -0500

    feat: Add the position island function to the game module
    Add the position island function to the game module to allow the user to position islands on each
    player's board

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -6,6 +6,8 @@ defmodule IslandsEngine.Game do
   use GenServer
   alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
 
+  @players [:player1, :player2]
+
   def start_link(name) when is_binary(name) do
     GenServer.start_link(__MODULE__, name, [])
   end
@@ -20,14 +22,39 @@ defmodule IslandsEngine.Game do
     GenServer.call(game, {:add_player, name})
   end
 
+  def position_island(game, player, key, row, col) when player in @players do
+    GenServer.call(game, {:position_island, player, key, row, col})
+  end
+
   def handle_call({:add_player, name}, _from, state) do
-    with {:ok, rules} <- Rules.check(state.rules, :add_player) do
+    case Rules.check(state.rules, :add_player) do
+      {:ok, rules} ->
+        state
+        |> update_player2_name(name)
+        |> update_rules(rules)
+        |> reply_success(:ok)
+
+      :error ->
+        {:reply, :error, state}
+    end
+  end
+
+  def handle_call({:position_island, player, key, row, col}, _from, state) do
+    board = player_board(state, player)
+
+    with {:ok, rules} <- Rules.check(state.rules, {:position_islands, player}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {:ok, island} <- Island.new(key, coordinate),
+         %{} = board <- Board.position_island(board, key, island) do
       state
-      |> update_player2_name(name)
+      |> update_board(player, board)
       |> update_rules(rules)
       |> reply_success(:ok)
     else
       :error -> {:reply, :error, state}
+      {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, state}
+      {:error, :invalid_island_type} -> {:reply, {:error, :invalid_island_type}, state}
+      {:error, :overlapping_island} -> {:reply, {:error, :overlapping_island}, state}
     end
   end
 
@@ -38,4 +65,9 @@ defmodule IslandsEngine.Game do
   defp update_rules(state, rules), do: %{state | rules: rules}
 
   defp reply_success(state, reply), do: {:reply, reply, state}
+
+  defp player_board(state, player), do: Map.get(state, player).board
+
+  defp update_board(state, player, board),
+    do: Map.update!(state, player, fn player -> %{player | board: board} end)
 end
